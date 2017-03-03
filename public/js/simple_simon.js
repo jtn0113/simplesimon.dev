dfa28310167174dfb06dc232e041c3d0affea668
John Nichols <jtn0113@yahoo.com>
2017-03-03 11:10:01 -0600

    updated simon game. fully functioning

--- public/js/simple_simon.js
+++ public/js/simple_simon.js
@@ -1,39 +1,75 @@
 var randomNumber;
-var turn = 4;
-var i = 0;
+var turn = 1;
+var clickedNumber = 0;
+var gamesColors = [];
+var myColors = [];
 function flash(buttonId) {
-	$(buttonId).animate({
+	console.log('buttonId is: ' + buttonId)
+	$("#" + buttonId).animate({
 		opacity: '1.0'
 	}, 500).animate({
 		opacity: '0.4'
 	}, 500)};
 
-// RANDOM NUMBER IS GENERATED 1-4, CORRESPONDING WITH COLORS OF BUTTONS
-function random() {randomNumber = Math.floor((Math.random() * 4) + 1);
-	if(randomNumber == 1) {
-			flash('#buttonGreen');
-	}
-	else if(randomNumber == 2) {
-			flash('#buttonRed');
-	}
-	else if(randomNumber == 3) {
-			flash('#buttonYellow');
-	}
-	else if(randomNumber == 4) {
-			flash('#buttonBlue');
-	};
+// RANDOM NUMBER IS GENERATED 1-4, NUMBER IS PUSHED TO GAMESCOLORS ARRAY
+function random() {
+	randomNumber = Math.floor((Math.random() * 4) + 1);
+	gamesColors.push(randomNumber);
+	gameSequence();
 };
 
-// BUTTON CLICKED WILL FLASH
-$('.button').click(function() {
-	flash(this);
+// NUMBER 1-4 TO COLORS, FLASHES THE COLOR
+function gameSequence() {
+	var i = 0;
+	var intervalId = setInterval(function() {
+		if(gamesColors[i] == 1) {
+				flash('buttonGreen');
+		}
+		else if(gamesColors[i] == 2) {
+				flash('buttonRed');
+		}
+		else if(gamesColors[i] == 3) {
+				flash('buttonYellow');
+		}
+		else if(gamesColors[i] == 4) {
+				flash('buttonBlue');
+		};
+
+		if(i == gamesColors.length) {
+			clearInterval(intervalId);
+			userInput();
+		}else{
+			i++;	
+		}
+	console.log(turn);
+	}, 1000);
+};
+
+// STARTS THE GAME
+$('#buttonMiddle').click(function(event) {
+	random();
 });
 
-// RANDOM SEQUENCE BASED ON WHAT TURN IT IS, 1 TURN = 1 FLASH
-var intervalId = setInterval(function() {
-	if(i >= turn - 1) {
-		clearInterval(intervalId)
-	}
-	random();
-	i++;
-	}, 1000);
+// BUTTON USER CLICKED WILL FLASH, IF CORRECT: CONTINUE
+function userInput() {
+	$('.button').on("click", function() {
+		console.log("click id: " + this.id);
+		flash(this.id);
+		console.log(this.id);
+		if($(this).attr('data') == gamesColors[clickedNumber]) {
+			console.log('Correct');
+			if(clickedNumber == gamesColors.length - 1) {
+				clickedNumber = 0;
+				random();
+			$('.button').off('click');
+			}else{
+				clickedNumber++;
+			}
+		} else {
+			gamesColors = [];
+			clickedNumber = 0;
+			$('.button').off('click');
+			console.log('Fail');
+		}
+	});
+};
