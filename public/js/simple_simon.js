752b6d3d65060cd21885c755c549ca3df4cd6d28
John Nichols <jtn0113@yahoo.com>
2017-03-07 09:44:52 -0600

    converted tabs to spaces

--- public/js/simple_simon.js
+++ public/js/simple_simon.js
@@ -1,73 +1,75 @@
-var randomNumber;
-var turn = 1;
-var clickedNumber = 0;
-var gamesColors = [];
-function flash(buttonId) {
-	$("#" + buttonId).animate({
-		opacity: '1.0'
-	}, 500).animate({
-		opacity: '0.4'
-	}, 500)};
+$(document).ready(function() {
+"use strict";
+    var randomNumber;
+    var turn = 1;
+    var clickedNumber = 0;
+    var gamesColors = [];
+    function flash(buttonId) {
+        $("#" + buttonId).animate({
+            opacity: '1.0'
+        }, 500).animate({
+            opacity: '0.4'
+        }, 500)};
 
-// RANDOM NUMBER IS GENERATED 1-4, NUMBER IS PUSHED TO GAMESCOLORS ARRAY
-function random() {
-	randomNumber = Math.floor((Math.random() * 4) + 1);
-	gamesColors.push(randomNumber);
-	gameSequence();
-};
+    // RANDOM NUMBER IS GENERATED 1-4, NUMBER IS PUSHED TO GAMESCOLORS ARRAY
+    function random() {
+        randomNumber = Math.floor((Math.random() * 4) + 1);
+        gamesColors.push(randomNumber);
+        gameSequence();
+    };
 
-// NUMBER 1-4 TO COLORS, FLASHES THE COLOR
-function gameSequence() {
-	var i = 0;
-	var intervalId = setInterval(function() {
-		if(gamesColors[i] == 1) {
-				flash('buttonGreen');
-		}
-		else if(gamesColors[i] == 2) {
-				flash('buttonRed');
-		}
-		else if(gamesColors[i] == 3) {
-				flash('buttonYellow');
-		}
-		else if(gamesColors[i] == 4) {
-				flash('buttonBlue');
-		};
+    // NUMBER 1-4 TO COLORS, FLASHES THE COLOR
+    function gameSequence() {
+        var i = 0;
+        var intervalId = setInterval(function() {
+            if(gamesColors[i] == 1) {
+                    flash('buttonGreen');
+            }
+            else if(gamesColors[i] == 2) {
+                    flash('buttonRed');
+            }
+            else if(gamesColors[i] == 3) {
+                    flash('buttonYellow');
+            }
+            else if(gamesColors[i] == 4) {
+                    flash('buttonBlue');
+            };
 
-		if(i == gamesColors.length) {
-			clearInterval(intervalId);
-			userInput();
-		}else{
-			i++;	
-		}
-	$("#score").html(gamesColors.length);
-	}, 1000);
-};
+            if(i == gamesColors.length) {
+                clearInterval(intervalId);
+                userInput();
+            }else{
+                i++;    
+            }
+        // ROUND COUNTER
+        $("#score").html(gamesColors.length);
+        }, 1000);
+    };
 
-// STARTS THE GAME
-$('#buttonMiddle').click(function(event) {
-	random();
-	$("#gameOver").html("");
+    // STARTS THE GAME
+    $('#buttonMiddle').click(function(event) {
+        random();
+        $("#gameOver").html("");
+    });
+
+    // BUTTON USER CLICKED WILL FLASH, IF CORRECT: CONTINUE
+    function userInput() {
+        $('.button').on("click", function() {
+            flash(this.id);
+            if($(this).attr('data') == gamesColors[clickedNumber]) {
+                if(clickedNumber == gamesColors.length - 1) {
+                    clickedNumber = 0;
+                    random();
+                $('.button').off('click');
+                }else{
+                    clickedNumber++;
+                }
+            } else {
+                gamesColors = [];
+                clickedNumber = 0;
+                $('.button').off('click');
+                $("#gameOver").html("GAME OVER");
+            }
+        });
+    };
 });
-
-// BUTTON USER CLICKED WILL FLASH, IF CORRECT: CONTINUE
-function userInput() {
-	$('.button').on("click", function() {
-		flash(this.id);
-		if($(this).attr('data') == gamesColors[clickedNumber]) {
-			if(clickedNumber == gamesColors.length - 1) {
-				clickedNumber = 0;
-				random();
-				$('#sound')[0].play();
-			$('.button').off('click');
-			}else{
-				clickedNumber++;
-				$('#sound')[0].play();
-			}
-		} else {
-			gamesColors = [];
-			clickedNumber = 0;
-			$('.button').off('click');
-			$("#gameOver").html("GAME OVER");
-		}
-	});
-};
